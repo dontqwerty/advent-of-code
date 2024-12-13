7d9429ad70f0430cef377fc32ede60290ffdd707
Giacomo Pilotti <jack.pilo04@gmail.com>
2024-12-13 08:59:28 +0100

    Implement virtualenv interpreter selection

--- utils/benchmark/benchmark.py
+++ utils/benchmark/benchmark.py
@@ -6,12 +6,38 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def execute_script(script_path, iterations=10):
-    # Change the working directory to the script's directory
-    script_directory = os.path.dirname(os.path.abspath(script_path))
-    script_name = os.path.basename(script_path)
-    os.chdir(script_directory)
+def find_venv_interpreter(dir_path):
+    # Potential virtual environment folder names
+    venv_folders = [".venv", ".env"]
 
+    # Potential interpreter location patterns
+    interpreter_paths = [
+        "bin/python",
+        "bin/python3",
+    ]
+
+    # Normalize the directory path
+    base_path = os.path.abspath(dir_path)
+
+    # Check each potential virtual environment folder
+    for venv_folder in venv_folders:
+        for interpreter_path in interpreter_paths:
+            # Construct full path to potential interpreter
+            potential_interpreter = os.path.join(
+                base_path, venv_folder, interpreter_path
+            )
+
+            # Check if the interpreter exists and is executable
+            if os.path.isfile(potential_interpreter) and os.access(
+                potential_interpreter, os.X_OK
+            ):
+                return potential_interpreter
+
+    # If no interpreter found
+    return None
+
+
+def execute_script(interpreter, script_name, iterations=10):
     durations_ms = []
     progress_bar = tqdm(total=iterations, desc="Running script")
     for i in range(iterations):
@@ -19,7 +45,12 @@ def execute_script(script_path, iterations=10):
         try:
             # Execute the script using subprocess
             subprocess.run(
-                ["python3", script_name], check=True, stdout=subprocess.DEVNULL
+                [
+                    interpreter,
+                    script_name,
+                ],
+                check=True,
+                stdout=subprocess.DEVNULL,
             )
         except subprocess.CalledProcessError as e:
             print(f"Execution failed on iteration {i+1}: {e}")
@@ -51,4 +82,15 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    execute_script(args.script_path, args.iterations)
+    # Change the working directory to the script's directory
+    script_directory = os.path.dirname(os.path.abspath(args.script_path))
+    script_name = os.path.basename(args.script_path)
+    os.chdir(script_directory)
+
+    venv_interpreter = find_venv_interpreter(script_directory)
+    interpreter = "python3" if venv_interpreter is None else venv_interpreter
+    print(f"Script: {script_directory}/{script_name}")
+    print(f"Interpreter: {interpreter}")
+    execute_script(
+        interpreter=interpreter, script_name=script_name, iterations=args.iterations
+    )
